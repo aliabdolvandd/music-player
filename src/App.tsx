fd498f725b4bb454eed36f11c24cd0467ae2566c
ali abdolvandd <aliabdolvandaa76@gmail.com>
2025-04-12 18:02:17 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-red-500"> sghg,sfgldfg</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
